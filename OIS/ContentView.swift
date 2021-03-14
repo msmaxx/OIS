8b4c2d9dbd33f721f61c42e7bf28234560bf5bd7
Maksim <42644986+msmaxx@users.noreply.github.com>
2021-03-14 16:02:50 +0200

    Initial Commit

---
+++ OIS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OIS
+//
+//  Created by root user on 14.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OIS/OISApp.swift
@@ -0,0 +1,17 @@
+//
+//  OISApp.swift
+//  OIS
+//
+//  Created by root user on 14.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct OISApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
